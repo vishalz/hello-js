e69c0d79ed200dd2351d18502380ef650f9c60ca
Vishal Arora <arorav@gmail.com>
2015-03-14 21:06:40 -0400

    added tests for arithmetic operator +

---
+++ ans/arithmetic.operators.b30.js
@@ -0,0 +1,130 @@
+
+module.exports = function(){ 
+
+/**
+*
+*@todo Declare variables  v3 , v3_type and  v3_value.  
+* Assign v3 =  10 + 20. 
+* Assign the value of v3 to variable v3_value as a string e.g. "40" or "5000" 
+* Assign the type of v3 to v3_type as a string e.g. "number" or "string" or "boolean"
+*
+*@todo Declare variables  v4 , v4_type and  v4_value.  
+* Assign v4 =  true + 10. 
+* Assign the value of v4 variable to v4_value as a string e.g. "40" or "5000" 
+* Assign the type of v4 to v4_type as a string e.g. "number" or "string" or "boolean"
+*@todo Declare variables  v5 , v5_type and  v5_value.  
+* Assign v5 =  false + 10. 
+* Assign the value of v5 variable to v5_value as a string e.g. "40" or "5000" 
+* Assign the type of v5 to v5_type as a string e.g. "number" or "string" or "boolean"
+*@todo Declare variables  v6 , v6_type and  v6_value.  
+* Assign v6 =  false + false. 
+* Assign the value of v6 variable to v6_value as a string e.g. "40" or "5000" 
+* Assign the type of v6 to v6_type as a string e.g. "number" or "string" or "boolean"
+*@todo Declare variables  v7 , v7_type and  v7_value.  
+* Assign v7 =  "foo"+ 10. 
+* Assign the value of v7 variable to v7_value as a string e.g. "40" or "5000" 
+* Assign the type of v7 to v7_type as a string e.g. "number" or "string" or "boolean"
+*@todo Declare variables  v8 , v8_type and  v8_value.  
+* Assign v8 =  "foo" + true. 
+* Assign the value of v8 variable to v8_value as a string e.g. "40" or "5000" 
+* Assign the type of v8 to v8_type as a string e.g. "number" or "string" or "boolean"
+*@todo Declare variables  v9 , v9_type and  v9_value.  
+* Assign v9 =  "foo" + false 
+* Assign the value of v9 variable to v9_value as a string e.g. "40" or "5000" 
+* Assign the type of v9 to v9_type as a string e.g. "number" or "string" or "boolean"
+*@todo Declare variables  v10 , v10_type and  v10_value.  
+* Assign v10 =  "foo" + "bar" 
+* Assign the value of v10 variable to v10_value as a string e.g. "40" or "5000" 
+* Assign the type of v10 to v10_type as a string e.g. "number" or "string" or "boolean"
+*@todo Declare variables  v11 , v11_type and  v11_value.  
+* Assign v11 =  . 
+* Assign the value of v11 variable to v11_value as a string e.g. "40" or "5000" 
+* Assign the type of v11 to v11_type as a string e.g. "number" or "string" or "boolean"
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
+// ******** Don't edit above this line *********************** 
+	var v3 = 10 + 20;
+  var v3_value = "30";
+	var v3_type = "number";
+
+	var v4 = true + 10;
+	var v4_value = "11";
+	var v4_type = "number";
+  
+	var v5 = false + 10;
+	var v5_value = "10";
+	var v5_type = "number";
+
+	var v6 = false + false ; 
+	var v6_value = "0";
+	var v6_type = "number";
+
+	var v7 = "foo" + 10 ; 
+	var v7_value = "foo10";
+	var v7_type = "string";
+
+  var v8 = "foo" + true; 
+	var v8_value = "footrue";
+	var v8_type = "string";
+
+  var v9 = "foo" + false; 
+	var v9_value = "foofalse";
+	var v9_type = "string";
+
+  var v10 = "foo" + "bar"; 
+	var v10_value = "foobar";
+	var v10_type = "string";
+
+
+
+
+
+
+
+
+// ******** Don't edit below this line *********************** 
+	var obj = {};
+	(typeof(v3)=== 'undefined') || (obj.v3 = v3); 
+	(typeof(v3_type)=== 'undefined') || (obj.v3_type = v3_type) ;
+	(typeof(v3_value)=== 'undefined') || (obj.v3_value = v3_value) ;
+	(typeof(v4)=== 'undefined') || (obj.v4 = v4);
+	(typeof(v4_type)=== 'undefined') || (obj.v4_type = v4_type) ;
+	(typeof(v4_value)=== 'undefined') || (obj.v4_value = v4_value) ;
+	(typeof(v5)=== 'undefined') || (obj.v5 = v5);
+	(typeof(v5_type)=== 'undefined') || (obj.v5_type = v5_type) ;
+	(typeof(v5_value)=== 'undefined') || (obj.v5_value = v5_value) ;
+	(typeof(v6)=== 'undefined') || (obj.v6 = v6) ;
+	(typeof(v6_value)=== 'undefined') || (obj.v6_value = v6_value) ;
+	(typeof(v6_type)=== 'undefined') || (obj.v6_type = v6_type) ;
+	(typeof(v7)=== 'undefined') || (obj.v7 = v7) ;
+	(typeof(v7_type)=== 'undefined') || (obj.v7_type = v7_type) ;
+	(typeof(v7_value)=== 'undefined') || (obj.v7_value = v7_value) ;
+	(typeof(v8)=== 'undefined') || (obj.v8 = v8) ;
+	(typeof(v8_type)=== 'undefined') || (obj.v8_type = v8_type) ;
+	(typeof(v8_value)=== 'undefined') || (obj.v8_value = v8_value) ;
+	(typeof(v9)=== 'undefined') || (obj.v9 = v9) ;
+	(typeof(v9_type)=== 'undefined') || (obj.v9_type = v9_type) ;
+	(typeof(v9_value)=== 'undefined') || (obj.v9_value = v9_value) ;
+	(typeof(v10)=== 'undefined') || (obj.v10 = v10) ;
+	(typeof(v10_type)=== 'undefined') || (obj.v10_type = v10_type) ;
+	(typeof(v10_value)=== 'undefined') || (obj.v10_value = v10_value) ;
+
+	return obj;
+}();
+
+
+
+
+
+
+
+
